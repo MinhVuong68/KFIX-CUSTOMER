5a27163cf4fce86ed42011b662a74f0354cb383c
VanTrinhDev0804 <vantrinh2804@gmail.com>
2023-03-16 22:20:22 +0700

    Authentication app

--- src/screens/Me/EditProfile/EditProfile.js
+++ src/screens/Me/EditProfile/EditProfile.js
@@ -4,7 +4,19 @@ import generalStyle from "../../../contains/styles";
 import { Button } from "../../../components";
 import styleProfile from "../Profile/stylesProfile";
 import Pressable from "react-native/Libraries/Components/Pressable/Pressable";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { updateAvatarUser, updateUserName } from "../../../redux/action";
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import { storage } from "../../../firebase/config";
 const EditProfile = () => {
+  const { user , isUpdate , loading } = useSelector((state) => state.auth);
+  const [image, setImage] = useState(user && user.avatar);
+
+  const [username, setUserName] = useState(user && user.username);
+  const [phoneNumber, setphoneNumber] = useState(user && user.phone);
+
+  const dispatch = useDispatch();
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
       allowsEditing: true,
@@ -12,19 +24,61 @@ const EditProfile = () => {
     });
 
     if (!result.canceled) {
-      console.log(result);
+      const uploadUrl = await uploadImageAsync(result.assets[0].uri);
+      setImage(uploadUrl);
     } else {
       alert("You did not select any image.");
     }
   };
 
+  async function uploadImageAsync(uri) {
+    // Why are we using XMLHttpRequest? See:
+    // https://github.com/expo/expo/issues/2402#issuecomment-443726662
+    const blob = await new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.onload = function () {
+        resolve(xhr.response);
+      };
+      xhr.onerror = function (e) {
+        console.log(e);
+        reject(new TypeError("Network request failed"));
+      };
+      xhr.responseType = "blob";
+      xhr.open("GET", uri, true);
+      xhr.send(null);
+    });
+
+    const fileRef = ref(storage, `user/${user.phone}`);
+    const result = await uploadBytes(fileRef, blob);
+
+    // We're done with the blob, close and release it
+    blob.close();
+
+    return await getDownloadURL(fileRef);
+  }
+
+  const handelUpdate = () => {
+    if (image !== user.avatar) {
+      dispatch(updateAvatarUser(image, phoneNumber));
+    }else if (username !== user.username){
+      dispatch(updateUserName(username, phoneNumber))
+    }else{
+      alert("Thông tin chưa có thay đổi !")
+    }
+  };
+  useEffect(()=>{
+    if(isUpdate){
+      alert('Đã thay đổi thông tin!')
+    }
+  },[loading])
+
   return (
     <View style={generalStyle.wrapper}>
-      <Pressable onPress={pickImageAsync  }>
+      <Pressable onPress={pickImageAsync}>
         <Image
           style={styleProfile.avatar}
           source={{
-            uri: "https://www.themoviedb.org/t/p/w500/blKKsHlJIL9PmUQZB8f3YmMBW5Y.jpg",
+            uri: image,
           }}
         />
       </Pressable>
@@ -38,6 +92,8 @@ const EditProfile = () => {
             fontSize: 20,
             marginBottom: 10,
           }}
+          value={username}
+          onChangeText={setUserName}
           placeholder="Tên của bạn"
         />
         <TextInput
@@ -48,12 +104,15 @@ const EditProfile = () => {
             fontSize: 20,
             marginBottom: 10,
           }}
+          value={phoneNumber}
+          onChangeText={setphoneNumber}
           placeholder="Số điện thoại"
           keyboardType="numeric"
+          editable={false}
         />
       </View>
 
-      <Button title="Thay đổi" />
+      <Button onPress={handelUpdate} title="Thay đổi" />
     </View>
   );
 };
